72f564a644b7f8ba29d57d87d1ba14458c417f53
shreyasnavada <vshreyas@adobe.com>
2024-12-22 12:33:01 +0530

    larger number program updatd

--- src/program_Practice/p3_printLargeNumberofArray.java
+++ src/program_Practice/p3_printLargeNumberofArray.java
@@ -1,30 +1,22 @@
 package program_Practice;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class p3_printLargeNumberofArray {
-  private static final String Break() = null;
-
 public static void main(String[] args) {
     // create an array
-    String[] myArray = {"plant", "apocalypse", "alert"};
-    int[] myArraynumbers = {1 , 3  , 500 , 50};
+    //String[] myArray = {"plant", "apocalypse", "alert"};
+    int[] myArraynumbers = {1 , 3  , 500 , 50, 501 , 390,498 , 1000, 1009 , 1 , 2, 1109, 2};
     int k = myArraynumbers.length;
-    System.out.println(k);
+    System.out.println("The sizeo of integer type array is:  " + k);
+    int j=0;
+    
     
     for(int i=0; i< k-1 ; i++)
     {
-    	if(i % 2 ==0)
+    	if(j< myArraynumbers[i])
     	{
-    		System.out.println(" " + i);
-    		Break();
-    		
-    	}
-    	else 
-    	{
-    		Break();
+    		j= myArraynumbers[i];
     	}
     }
-    }
+    System.out.println("The largest number from an array is: " + " " +j);
   }
+}
