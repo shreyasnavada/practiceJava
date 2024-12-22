9e16e5b6f5696656b62f419522ad30e71d59eafa
shreyasnavada <vshreyas@adobe.com>
2024-12-22 12:37:37 +0530

    small number program updatded

---
+++ src/program_Practice/p4_printSmallNumberofArray.java
@@ -0,0 +1,22 @@
+package program_Practice;
+
+public class p4_printSmallNumberofArray {
+public static void main(String[] args) {
+    // create an array
+    //String[] myArray = {"plant", "apocalypse", "alert"};
+    int[] myArraynumbers = {10 , 3  , 500 , 50, 501 , 390,498 , 1000, 1009 , 1 , 2, 1109, 2};
+    int k = myArraynumbers.length;
+    System.out.println("The sizeo of integer type array is:  " + k);
+    int j= myArraynumbers[0];
+    
+    
+    for(int i=0; i< k-1 ; i++)
+    {
+    	if(j> myArraynumbers[i])
+    	{
+    		j= myArraynumbers[i];
+    	}
+    }
+    System.out.println("The largest number from an array is: " + " " +j);
+  }
+}
