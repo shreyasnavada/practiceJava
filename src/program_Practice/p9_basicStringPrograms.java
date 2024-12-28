5b9d70e100d03750f5c0174626a5b32cf1159f31
shreyasnavada <snavada94@gmail.com>
2024-12-28 11:07:23 +0530

    Updated program

--- src/program_Practice/p9_basicStringPrograms.java
+++ src/program_Practice/p9_basicStringPrograms.java
@@ -45,11 +45,53 @@ public class p9_basicStringPrograms {
 
 	
 //	program to check whether a string is a Palindrome 
-	
+//String Program
 	public static void main(String[] args) {
-		//added new comment
-	
-		
+		String s1 = "Rahul Shetty Academy";
 
-	}
-	}
+		String s5 = "hello";
+
+		String s2 = new String("Welcome");
+
+		String s3 = new String("Welcome");// here the String s3 points to same location of s2.
+
+		String s = "Rahul Shetty Academy";
+
+		String[] splittedString = s.split("Shetty");// value is stored in array where the split takes at shetty word
+		String[] splitStringA = s.split(" ");
+
+		System.out.println(splittedString[0]);
+		System.out.println(splitStringA[0]);
+
+		System.out.println(splittedString[1]);
+
+		System.out.println(splittedString[1].trim());
+
+		for(int i =s.length()-1;i>=0;i--)
+
+		{
+
+		System.out.println(s.charAt(i));
+
+		}
+	        // input string having spaces, comma etc.
+	        String s0 = "This is,comma.fullstop  whitespace";
+	        // The regex [,\\.\\s] splits the string by
+	        // commas (,), spaces (\\s), and periods (\\.)
+	        String regex = "[,\\s\\.]";
+
+	        // using split() method
+	        String[] arr = s0.split(regex);
+
+	        // Print each element of the resulting array
+	        for (String s9 : arr) {
+	            System.out.println(s9);
+	        }
+	        System.out.println(s0.length());
+	        
+	        for(int j = s0.length()-1; j>=0 ; j--)
+	        {
+	        	System.out.println(s0.charAt(j));
+	        }
+	    }
+}
