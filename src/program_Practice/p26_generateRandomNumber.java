ceaf208b45c3db2f68304da32425f63860f3a441
shreyasnavada <snavada94@gmail.com>
2025-01-03 11:59:04 +0530

    added programs

--- src/program_Practice/p26_generateRandomNumber.java
+++ src/program_Practice/p26_generateRandomNumber.java
@@ -1,10 +1,57 @@
 package program_Practice;
 
 public class p26_generateRandomNumber {
+	
+	   // Function to find the biggest of three numbers using Iternary Operator
+	   public  static int biggestOfThree(int x, int y, int z)
+	    {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	        return z > (x > y ? x : y) ? z : ((x > y) ? x : y);
+	    }
 
+	    // Main driver function
+	    public static void main(String[] args)
+	    {
+
+	        // Declaring variables for 3 numbers
+	        int a, b, c;
+
+	        // Variable holding the largest number
+	        int largest;
+	        int largest1;
+	        a = 5;
+	        b = 10;
+	        c = 3;
+	        // Calling the above function in main
+	        largest = biggestOfThree(a, b, c);
+	        //biggestOfThree
+	        largest1 = biggestOfThree1(a, b, c);
+
+	        // Printing the largest number
+	        System.out.println(largest
+	                           + " is the largest number using iternary operator.");
+	        System.out.println(largest1 + " is the largest number using if else condition.");
+	    }
+	    //Using If else condition
+	    static int biggestOfThree1(int x, int y, int z)
+	    {
+
+	        // Comparing all 3 numbers
+	        if (x >= y && x >= z)
+
+	            // Returning 1st number if largest
+	            return x;
+
+	        // Comparing 2nd no with 1st and 3rd no
+	        else if (y >= x && y >= z)
+
+	            // Return z if the above conditions are false
+	            return y;
+
+	        else
+
+	            // Returning 3rd no, Its sure it is greatest
+	            return z;
+	    }
+	    
 	}
-
-}
---
+++ src/program_Practice/p27_printlargestof3Numbers.java
@@ -0,0 +1,10 @@
+package program_Practice;
+
+public class p27_printlargestof3Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
