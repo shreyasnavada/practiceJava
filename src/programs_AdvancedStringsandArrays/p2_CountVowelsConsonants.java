95deea846c59aa4e92ddd423f370a1174876de3e
shreyasnavada <snavada94@gmail.com>
2025-01-30 23:40:19 +0530

    Chnage 2

---
+++ src/programs_AdvancedStringsandArrays/p2_CountVowelsConsonants.java
@@ -0,0 +1,12 @@
+package programs_AdvancedStringsandArrays;
+
+public class p2_CountVowelsConsonants {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1= new String("  Hari has a Sandwich ");
+		System.out.println(s1);
+
+	}
+
+}
